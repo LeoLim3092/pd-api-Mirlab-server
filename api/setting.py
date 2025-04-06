bf104d02b73beee7b79fca7219c65f6c174096ee
LeoLim3092 <leo.lim@mirlab.org>
2025-04-06 13:22:10 +0800

    corrected the setting.py in mysite

--- api/setting.py
+++ api/setting.py
@@ -1,22 +1,3 @@
-from pathlib import Path
-
 MEDIA_URL = '/mnt/pd_app/results/'
 DEBUG = True
 APPEND_SLASH = True
-BASE_DIR = Path('/home/pdapp/pd_api_server')
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],  # Add this line
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
