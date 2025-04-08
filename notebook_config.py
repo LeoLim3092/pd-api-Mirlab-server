78a87660f37a4b97f4299e246e1b0d5f57cde286
LeoLim3092 <leo.lim@mirlab.org>
2025-04-08 13:44:32 +0800

    correct the project name

--- notebook_config.py
+++ notebook_config.py
@@ -2,5 +2,5 @@ import os
 import django
 
 # Set up Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pd_api_server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
